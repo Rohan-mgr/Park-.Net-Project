29e057fdc84dec329a1382f7979a684edc66f4bc
Rohan Magar <rohan.magar.415@gmail.com>
2022-07-17 17:11:54 +0545

    updated admin functionality

--- DMS/DMS.UI/Controllers/AdminPanelController.cs
+++ DMS/DMS.UI/Controllers/AdminPanelController.cs
@@ -23,12 +23,21 @@ namespace DMS.Controllers
             Session["count"] = db.User_Message.Count();
             //List<Car_Rented> rentCar = db.Car_Rented.ToList();
             Session["rentCount"] = db.Car_Rented.Count();
+            Session["sum"] = db.Car_Rented.Sum(x => x.t_amount);
+            Session["netsum"] = db.Car_Rented.Sum(x => x.t_amount) - db.Car_Rented.Sum(x => x.t_amount) * 0.05;
             return View(data);
         }   
         public ActionResult Chart()
         {
             List<Car_Rented> data = db.Car_Rented.ToList();
             return View(data);
+        }
+        public ActionResult Delete(int id)
+        {
+            Car_Rented data = db.Car_Rented.Find(id);
+            db.Car_Rented.Remove(data);
+            db.SaveChanges();
+            return RedirectToAction("Chart");
         }
         public ActionResult Gallery()
         {
